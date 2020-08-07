3dd2599347e6a31db743e08d649160f6167177dd
gaurav <270mehta@gmail.com>
2020-08-07 16:33:11 +0530

    Added All movie links

---
+++ lib/model/movie.dart
@@ -0,0 +1,226 @@
+class Movie {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final String description;
+  final String rating;
+  final String year;
+  final String duration;
+  final String trailer;
+  final String movie;
+
+  Movie(
+      {this.id,
+      this.title,
+      this.imageUrl,
+      this.description,
+      this.rating,
+      this.year,
+      this.duration,
+      this.trailer,
+      this.movie});
+}
+
+final movieList = [
+  Movie(
+      id: 'tt4154796',
+      title: 'Avengers: Endgame',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+      description:
+          'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse...',
+      rating: '8.6',
+      year: '2019',
+      duration: '181 min',
+      trailer: 'https://www.youtube.com/watch?v=TcMBFSGVi1c',
+      movie:
+          'http://com.tdbot5.workers.dev/4:/Avengers%20Endgame%20(2019)[Proper%201080p%20HDRip%20-%20HQ%20Line%20Audios%20-%20[Tamil%20Telugu%20Hin%20Eng]%20-%20x264%20-%203.2GB][Moviefreak.xyz][Moviefreak.xyz].mkv'),
+  Movie(
+      id: 'tt7286456',
+      title: 'Joker',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg',
+      description:
+          'A gritty character study of Arthur Fleck, a man disregarded by society.',
+      rating: '9.1',
+      year: '2019',
+      duration: '121 min',
+      trailer: 'https://www.youtube.com/watch?v=-_DJEzZk2pc',
+      movie:
+          'https://drive.skyrim.workers.dev/Joker%2520(2019)%2520%5B2160p%5D%2520%5B4K%5D%2520%5BBluRay%5D%2520%5BHDR%5D%2520%5B5.1%5D%2520%5BYTS.LT%5D/Joker.2019.2160p.4K.BluRay.x265.10bit.HDR.AAC5.1-%5BYTS.LT%5D.mkv'),
+  Movie(
+      id: 'tt9248972',
+      title: 'Mission Mangal',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BZThmMWFlOWYtY2M0YS00OWIxLWI4MzgtYTJhMDNkZDY4ZjhjXkEyXkFqcGdeQXVyNzc4NzEzNjY@._V1_SX300.jpg',
+      description:
+          'Based on true events of the Indian Space Research Organisation (ISRO) successfully launching the Mars Orbiter Mission (Mangalyaan), making it the least expensive mission to Mars.',
+      rating: '6.6',
+      year: '2019',
+      duration: '130 min',
+      trailer: 'https://www.youtube.com/watch?v=q10nfS9V090',
+      movie:
+          'http://com.tdbot2.workers.dev/1:/Mission.Mangal.2019.1080p.@NetFlixPrimeMovies.mkv'),
+  Movie(
+      id: 'tt7349950',
+      title: 'It: Chapter Two',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BYTJlNjlkZTktNjEwOS00NzI5LTlkNDAtZmEwZDFmYmM2MjU2XkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg',
+      description:
+          'Twenty-seven years after their first encounter with the terrifying Pennywise, the Losers Club have grown up and moved away, until a devastating phone call brings them back.',
+      rating: '7.0',
+      year: '2019',
+      duration: '169 min',
+      trailer: 'https://www.youtube.com/watch?v=xhJ5P7Up3jA',
+      movie:
+          'https://drive.bartixxx.workers.dev/It%20Chapter%20Two%20(2019)%20[WEBRip]%20[1080p]%20[YTS.LT]/It.Chapter.Two.2019.1080p.WEBRip.x264-[YTS.LT].mp4'),
+  Movie(
+      id: 'tt6806448',
+      title: 'Hobbs & Shaw',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg',
+      description:
+          'Lawman Luke Hobbs and outcast Deckard Shaw form an unlikely alliance when a cyber-genetically enhanced villain threatens the future of humanity.',
+      rating: '6.7',
+      year: '2019',
+      duration: '137 min',
+      trailer: 'https://www.youtube.com/watch?v=HZ7PAyCDwEg',
+      movie:
+          'https://td.klprojects.tech/0:/Fast.and.Furious.Presents.Hobbs.and.Shaw.2019.1080p.BluRay.1600MB.DD5.1.x264-GalaxyRG[TGx]/Fast.and.Furious.Presents.Hobbs.and.Shaw.2019.1080p.BluRay.1600MB.DD5.1.x264-GalaxyRG.mkv'),
+  Movie(
+      id: 'tt6105098',
+      title: 'The Lion King',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg',
+      description:
+          'After the murder of his father, a young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.',
+      rating: '7.1',
+      year: '2019',
+      duration: '118 min',
+      trailer: 'https://www.youtube.com/watch?v=7TavVZMewpY',
+      movie:
+          'https://td.klprojects.tech/0:/www.1TamilMV.org%20-%20The%20Lion%20King%20(2019)%20BluRay%20-%201080p%20-%20v2%20Org%20Auds%20[Tel%20+%20Tam%20+%20Hin%20+%20Eng].mkv'),
+];
+
+final topRatedMovieList = [
+  Movie(
+      id: 'tt1375666',
+      title: 'Inception',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+      description:
+          'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+      rating: '8.8',
+      year: '2010',
+      duration: '148 min',
+      trailer: 'https://www.youtube.com/watch?v=YoHD9XEInc0',
+      movie:
+          'https://td.klprojects.tech/0:/Inception%20(2010)%20[1080p]/Inception.2010.1080p.BrRip.x264.YIFY.mp4'),
+  Movie(
+      id: 'tt0468569',
+      title: 'The Dark Knight',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg',
+      description:
+          'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+      rating: '9.0',
+      year: '2008',
+      duration: '152 min',
+      trailer: 'https://www.youtube.com/watch?v=YoHD9XEInc0',
+      movie:
+          'https://td.klprojects.tech/0:/The%20Dark%20Knight%20Trilogy%201080p%2010bit%20BluRay%20x265%20HEVC%206CH-MRN/The.Dark.Knight.2008.IMAX.1080p.10bit.BluRay.x265.HEVC.6CH-MRN.mkv'),
+  Movie(
+      id: 'tt0816692',
+      title: 'Interstellar',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+      description:
+          'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.',
+      rating: '8.6',
+      year: '2014',
+      duration: '169 min',
+      trailer: 'https://www.youtube.com/watch?v=zSWdZVtXT7E',
+      movie:
+          'https://td.klprojects.tech/0:/Interstellar%20(2014)%20(2014)%20[1080p]/Interstellar.2014.2014.1080p.BluRay.x264.YIFY.mp4?a=view'),
+  Movie(
+      id: 'tt4633694',
+      title: 'Spider-Man: Into the Spider-Verse',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_SX300.jpg',
+      description:
+          'Teen Miles Morales becomes Spider-Man of his reality, crossing his path with five counterparts from other dimensions to stop a threat for all realities.',
+      rating: '8.4',
+      year: '2018',
+      duration: '117 min',
+      trailer: 'https://www.youtube.com/watch?v=zSWdZVtXT7E',
+      movie:
+          'https://be1e0759364f1c8927c8e2572dc2203a.belecialialionmearth.workers.dev/0:/Spider-Man%20Into%20The%20Spider-Verse%20(2018)%20[BluRay]%20[3D]%20[HSBS]%20[YTS.AM]/Spider-Man.Into.The.Spider-Verse.2018.3D.HSBS.BluRay.x264-[YTS.AM].mp4'),
+  Movie(
+      id: 'tt1187043',
+      title: '3 Idiots',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BNTkyOGVjMGEtNmQzZi00NzFlLTlhOWQtODYyMDc2ZGJmYzFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+      description:
+          'Two friends are searching for their long lost companion. They revisit their college days and recall the memories of their friend who inspired them to think differently, even as the rest of the world called them \"idiots\".',
+      rating: '8.4',
+      year: '2009',
+      duration: '170 min',
+      trailer: 'https://www.youtube.com/watch?v=K0eDlFX9GMc',
+      movie:
+          'https://be1e0759364f1c8927c8e2572dc2203a.belecialialionmearth.workers.dev/0:/3%20Idiots%202009%201080p%20BluRay%20x264%20Hindi%20AAC%20-%20Ozlem/3%20Idiots%202009%201080p%20BluRay%20x264%20Hindi%20AAC%20-%20Ozlem.mp4'),
+  Movie(
+      id: 'tt1049413',
+      title: 'Up',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BMTk3NDE2NzI4NF5BMl5BanBnXkFtZTgwNzE1MzEyMTE@._V1_SX300.jpg',
+      description:
+          'Seventy-eight year old Carl Fredricksen travels to Paradise Falls in his home equipped with balloons, inadvertently taking a young stowaway.',
+      rating: '8.2',
+      year: '2009',
+      duration: '96 min',
+      trailer: 'https://www.youtube.com/watch?v=ORFWdXl_zJ4',
+      movie:
+          'https://be1e0759364f1c8927c8e2572dc2203a.belecialialionmearth.workers.dev/0:/Disney%20Animated%20Collection%20hevc-d3g%20(yusuo)/Up%202009%20BluRay%201080p%20DD5.1%20H265-d3g.mkv'),
+];
+
+final bestMovieList = [
+  Movie(
+      id: 'tt0437086',
+      title: 'Alita: Battle Angel',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BNzVhMjcxYjYtOTVhOS00MzQ1LWFiNTAtZmY2ZmJjNjIxMjllXkEyXkFqcGdeQXVyNTc5OTMwOTQ@._V1_SX300.jpg',
+      description:
+          'A deactivated cyborg is revived, but cannot remember anything of her past life and goes on a quest to find out who she is.',
+      rating: '7.4',
+      year: '2019',
+      duration: '122 min',
+      trailer: 'https://www.youtube.com/watch?v=w7pYhpJaJW8',
+      movie:
+          'https://be1e0759364f1c8927c8e2572dc2203a.belecialialionmearth.workers.dev/0:/Alita%20Battle%20Angel%20(2019)%20[WEBRip]%20[720p]%20[YTS.LT]/Alita.Battle.Angel.2019.720p.WEBRip.x264-[YTS.LT].mp4'),
+  Movie(
+      id: 'tt6320628',
+      title: 'Spider-Man: Far from Home',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg',
+      description:
+          'Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.',
+      rating: '7.7',
+      year: '2019',
+      duration: '129 min',
+      trailer: 'https://www.youtube.com/watch?v=Nt9L1jCKGnE',
+      movie:
+          'https://be1e0759364f1c8927c8e2572dc2203a.belecialialionmearth.workers.dev/0:/Spider-Man%20Far%20From%20Home%20(2019)%20[WEBRip]%20[720p]%20[YTS.LT]/Spider-Man.Far.From.Home.2019.720p.WEBRip.x264-[YTS.LT].mp4'),
+  Movie(
+      id: 'tt1979376',
+      title: 'Toy Story 4',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BMTYzMDM4NzkxOV5BMl5BanBnXkFtZTgwNzM1Mzg2NzM@._V1_SX300.jpg',
+      description:
+          'When a new toy called \"Forky\" joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy.',
+      rating: '8.1',
+      year: '2019',
+      duration: '100 min',
+      trailer: 'https://www.youtube.com/watch?v=wmiIUN-7qhE',
+      movie:
+          'https://be1e0759364f1c8927c8e2572dc2203a.belecialialionmearth.workers.dev/0:/www.TamilRockers.ws%20-%20Toy%20Story%204%20(2019)[1080p%20BDRip%20-%20Org%20Auds%20-%20[Tamil%20+%20Telugu%20+%20Hin%20+%20Eng]%20-%20x264%20-%201.7GB%20-%20ESubs].mkv'),
+];
